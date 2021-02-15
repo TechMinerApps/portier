b429121b334421a926d434a25d5dba965947d165
Easton Man <manyang.me@outlook.com>
2021-02-15 22:06:50 +0800

    feat(bot): add bot start stop command

--- modules/bot/bot.go
+++ modules/bot/bot.go
@@ -6,6 +6,25 @@ type App struct {
 	Bot *telebot.Bot
 }
 
+func NewTeleBot() (*App, error) {
+	var app App
+	var err error
+	app.Bot, err = telebot.NewBot(telebot.Settings{})
+	if err != nil {
+		return nil, err
+	}
+
+	return &app, nil
+}
+
+func (app *App) Start() {
+	app.Bot.Start()
+}
+
+func (app *App) Stop() {
+	app.Bot.Stop()
+}
+
 func (app *App) configCommands() {
 	app.Bot.Handle("/start", app.cmdStart)
 }
