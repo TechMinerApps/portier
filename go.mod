8277854e4fe7d362965f431c1c39002ead3bf2e8
Easton Man <manyang.me@outlook.com>
2021-03-15 20:57:59 +0800

    refactor(telegraph): change telegraph library

--- go.mod
+++ go.mod
@@ -4,12 +4,12 @@ go 1.15
 
 require (
 	github.com/PuerkitoBio/goquery v1.6.1 // indirect
+	github.com/TechMinerApps/telegraph v0.2.0
 	github.com/andybalholm/cascadia v1.2.0 // indirect
 	github.com/fsnotify/fsnotify v1.4.9 // indirect
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/magiconair/properties v1.8.4 // indirect
 	github.com/mattn/go-sqlite3 v1.14.6 // indirect
-	github.com/meinside/telegraph-go v0.0.3
 	github.com/mitchellh/mapstructure v1.4.1 // indirect
 	github.com/mmcdole/gofeed v1.1.0
 	github.com/mmcdole/goxpp v0.0.0-20200921145534-2f3784f67354 // indirect
@@ -20,19 +20,15 @@ require (
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5
 	github.com/spf13/viper v1.7.1
-	github.com/stretchr/testify v1.7.0 // indirect
 	github.com/tidwall/btree v0.4.2 // indirect
 	github.com/tidwall/buntdb v1.2.0
 	github.com/tidwall/pretty v1.1.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 	go.uber.org/zap v1.16.0
-	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
-	golang.org/x/sys v0.0.0-20210309074719-68d13333faf2 // indirect
-	golang.org/x/text v0.3.5 // indirect
+	golang.org/x/sys v0.0.0-20210314195730-07df6a141424 // indirect
 	gopkg.in/ini.v1 v1.62.0 // indirect
 	gopkg.in/tucnak/telebot.v2 v2.3.5
 	gopkg.in/yaml.v2 v2.4.0 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 	gorm.io/driver/mysql v1.0.5
 	gorm.io/driver/sqlite v1.1.4
 	gorm.io/gorm v1.21.3
--- modules/telegraph/telegraph.go
+++ modules/telegraph/telegraph.go
@@ -1,16 +1,13 @@
 package telegraph
 
 import (
+	"errors"
 	"sync"
 	"time"
 
 	"github.com/TechMinerApps/portier/models"
 	"github.com/TechMinerApps/portier/modules/log"
-	tgraph "github.com/meinside/telegraph-go"
-)
-
-const (
-	ErrTelegraphFlood = iota
+	tgraph "github.com/TechMinerApps/telegraph"
 )
 
 type Telegraph interface {
@@ -18,6 +15,8 @@ type Telegraph interface {
 	// Publish is a blocking function that insert the provided feed into queue and wait for process
 	Publish(item *models.Feed) (string, error)
 
+	// Start is used to start a instance
+	// telegraph do not needs to stop, just close the input channel
 	Start()
 }
 
@@ -39,7 +38,7 @@ type Item struct {
 type telegraph struct {
 	logger        log.Logger
 	config        *Config
-	clientPool    []*tgraph.Client
+	clientPool    []tgraph.Client
 	currentClient int
 	lock          sync.Mutex
 	queue         chan *Item
@@ -47,22 +46,29 @@ type telegraph struct {
 
 func NewTelegraph(c *Config) (Telegraph, error) {
 	t := &telegraph{
-		logger:        c.Logger,
-		config:        c,
-		clientPool:    []*tgraph.Client{},
+		logger: c.Logger,
+		config: c,
+
+		// Load balance with round-robin
+		clientPool:    []tgraph.Client{},
 		currentClient: 0,
-		lock:          sync.Mutex{},
-		queue:         make(chan *Item),
+
+		// Mutex lock to protect currentClient
+		lock:  sync.Mutex{},
+		queue: make(chan *Item),
 	}
 
+	// If newly created instance
 	if len(c.AccessToken) == 0 {
 		if err := t.createAccount(); err != nil {
 			return nil, err
 		}
 		return t, nil
 	}
+
+	// Spawn clients from access token
 	for _, token := range c.AccessToken {
-		client, err := tgraph.Load(token)
+		client, err := tgraph.NewClientWithToken(token)
 		if err != nil {
 			return nil, err
 		}
@@ -73,13 +79,25 @@ func NewTelegraph(c *Config) (Telegraph, error) {
 
 func (t *telegraph) createAccount() error {
 	for i := 0; i < t.config.AccountNumber; i++ {
-		client, err := tgraph.Create(t.config.ShortName, t.config.AuthorName, t.config.AuthorURL)
+		AccountInfo := tgraph.Account{
+			AccessToken: "",
+			AuthURL:     "",
+			ShortName:   t.config.ShortName,
+			AuthorName:  t.config.AuthorName,
+			AuthorURL:   t.config.AuthorURL,
+			PageCount:   0,
+		}
+		client := tgraph.NewClient()
+		_, err := client.CreateAccount(AccountInfo)
 		if err != nil {
 			return err
 		}
 		t.clientPool = append(t.clientPool, client)
-		t.config.AccessToken = append(t.config.AccessToken, client.AccessToken)
+		t.config.AccessToken = append(t.config.AccessToken, client.Account().AccessToken)
 		t.logger.Infof("Created Telegraph account %d success", i)
+
+		// Avoid flood wait
+		// Can lead to long waiting at first time start
 		time.Sleep(time.Second)
 	}
 	return nil
@@ -93,9 +111,15 @@ func (t *telegraph) Start() {
 				if err == nil {
 					item.ResultChan <- url
 					break
+				} else if err == tgraph.ErrFloodWait {
+					t.logger.Warnf("Recieve Telegraph flood wait: %s", err.Error())
+
+					// Flood wait 7s, but wait a longer 10 seconds to ensure success
+					time.Sleep(10 * time.Second)
 				} else {
-					t.logger.Warnf("Error publishing to telegraph: %s", err.Error())
-					time.Sleep(60 * time.Second)
+					t.logger.Errorf("Error publishing to telegraph: %s", err.Error())
+					item.ResultChan <- ""
+					break
 				}
 			}
 
@@ -103,8 +127,10 @@ func (t *telegraph) Start() {
 	}()
 }
 
+// Publish is a blocking function that wait for the page to return or return a error
 func (t *telegraph) Publish(feed *models.Feed) (string, error) {
 	resultCh := make(chan string)
+	defer close(resultCh)
 	item := &Item{
 		ResultChan: resultCh,
 		Feed:       feed,
@@ -114,9 +140,10 @@ func (t *telegraph) Publish(feed *models.Feed) (string, error) {
 	t.queue <- item
 
 	// Wait for result
-
 	url := <-resultCh
-	close(resultCh)
+	if url == "" {
+		return "", errors.New("receiving empty url, may be error")
+	}
 
 	return url, nil
 }
@@ -125,28 +152,38 @@ func (t *telegraph) publish(item *Item) (string, error) {
 
 	// publish should not mess with channel
 
+	// content directly from feed
 	htmlContent := item.Feed.Item.Content
 	if htmlContent == "" {
+		// Must have content
 		htmlContent = "Empty Content"
 	}
-	var err error = nil
-	var url string = ""
-	t.lock.Lock()
-	if page, err1 := t.clientPool[t.currentClient].CreatePageWithHTML(
-		item.Feed.Item.Title,
-		item.Feed.Item.Author.Name,
-		item.Feed.Item.Link,
-		htmlContent,
-		true); err1 == nil {
-		url = page.URL
 
+	// use mutex lock to avoid concurrent use of same client
+	t.lock.Lock()
+	defer t.lock.Unlock()
+	defer func() {
+		t.currentClient++
+		if t.currentClient == len(t.clientPool) {
+			t.currentClient = 0
+		}
+	}()
+
+	// Create a new page
+	content, err := t.clientPool[t.currentClient].ContentFormat(htmlContent)
+	if err != nil {
+		return "", err
+	}
+	page := tgraph.Page{
+		Title:       item.Feed.Item.Title,
+		Description: item.Feed.Item.Description,
+		AuthorName:  item.Feed.Item.Author.Name,
+		AuthorURL:   item.Feed.Item.Link,
+		Content:     content,
+	}
+	if page, err := t.clientPool[t.currentClient].CreatePage(page, false); err != nil {
+		return "", err
 	} else {
-		err = err1
+		return page.URL, nil
 	}
-	t.currentClient++
-	if t.currentClient == len(t.clientPool) {
-		t.currentClient = 0
-	}
-	t.lock.Unlock()
-	return url, err
 }
